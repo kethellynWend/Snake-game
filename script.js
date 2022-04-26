9aa244054fb0a4b5b75422c8b644e387943121f5
kethellyn wend <kethellynwend.1@gmail.com>
2022-04-26 17:05:34 -0300

    criar comidinha

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ snake[0] = {
 };
 
 let direction = "rigth";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
 function criarBG() {
   context.fillStyle = "lightgreen";
   context.fillRect(0, 0, 16 * box, 16 * box);
@@ -17,6 +22,11 @@ function criarCobrinha() {
     context.fillStyle = "green";
     context.fillRect(snake[i].x, snake[i].y, box, box);
   }
+}
+
+function drawFood(){
+  context.fillStyle = "red";
+  context.fillRect(food.x, food.y, box, box );
 }
 
 document.addEventListener("keydown", update);
@@ -38,6 +48,7 @@ function iniciarJogo() {
 
   criarBG();
   criarCobrinha();
+  drawFood();
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
