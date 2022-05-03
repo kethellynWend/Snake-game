c91c73ed634f10cbae46a70a0ebaac866777e5f7
kethellyn wend <kethellynwend.1@gmail.com>
2022-05-03 17:44:30 -0300

    personalizei um pouco

--- script.js
+++ script.js
@@ -14,18 +14,18 @@ let food = {
 };
 
 function criarBG() {
-  context.fillStyle = "lightgreen";
+  context.fillStyle = "pink";
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
 function criarCobrinha() {
   for (i = 0; i < snake.length; i++) {
-    context.fillStyle = "green";
+    context.fillStyle = "purple";
     context.fillRect(snake[i].x, snake[i].y, box, box);
   }
 }
 
 function drawFood() {
-  context.fillStyle = "red";
+  context.fillStyle = "green";
   context.fillRect(food.x, food.y, box, box);
 }
 
